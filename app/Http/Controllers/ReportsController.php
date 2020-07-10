5edfc816bf30fa9ddfb1407f1f751cd905c928f3
Walter Indeche <walter.indeche@andela.com>
2020-07-10 12:31:08 +0300

    Report Fix

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -60,23 +60,27 @@ class ReportsController extends Controller
       return $this->sendSuccessResponse($total->toArray());
     }
 
+
     public function totalsByAge()
     {
         $first =[10,14];
         $second =[15,19];
         $third =[20,24];
         $fourth =[25,30];
+        $fifth =[31,100];
 
         $ages_1= Youth::whereBetween('age',$first)->count();
         $ages_2= Youth::whereBetween('age',$second)->count();
         $ages_3= Youth::whereBetween('age',$third)->count();
         $ages_4= Youth::whereBetween('age',$fourth)->count();
+        $ages_5= Youth::whereBetween('age',$fifth)->count();
 
         $data=[
                 ['count'=>$ages_1,'key'=>'10-14'],
                 ['count'=>$ages_2,'key'=>'15-19'],
                 ['count'=>$ages_3,'key'=>'20-24'],
                 ['count'=>$ages_4,'key'=>'25-30'],
+                ['count'=>$ages_5,'key'=>'Above 30'],
             ];
         return $this->sendSuccessResponse($data);
     }
